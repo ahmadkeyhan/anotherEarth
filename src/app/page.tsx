446eea1224d09a69e303cde5f1b69ca97b9bee11
keyhansa <keyhan.ahmad@gmail.com>
2025-02-27 09:07:22 +0330

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import MetaplexLogo from "@/assets/logos/metaplex-logo.png";
 import Header from "@/components/header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <Header />
+      <p>An on-chain quest to find another earth, before it's too late! 🧑‍🚀🚀</p>
     </main>
   );
 }
