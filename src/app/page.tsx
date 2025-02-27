1712a9bd9ee59b71b877f591c3d2c2a5ee814d8c
keyhansa <keyhan.ahmad@gmail.com>
2025-02-27 09:14:52 +0330

    fixed the single quote problem caused by emojis!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <Header />
-      <p>An on-chain quest to find another earth, before it's too late! 🧑‍🚀🚀</p>
+      <p>{`An on-chain quest to find another earth, before it's too late! 🧑‍🚀🚀`}</p>
     </main>
   );
 }
