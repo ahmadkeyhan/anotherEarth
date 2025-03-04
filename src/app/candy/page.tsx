80c9b9dafef37547d7a362bc536493591f87130a
keyhansa <keyhan.ahmad@gmail.com>
2025-03-04 12:52:47 +0330

    imported and used MintOne component / omminted several instancing of umi

--- src/app/candy/page.tsx
+++ src/app/candy/page.tsx
@@ -1,52 +1,13 @@
 "use client"
 import Header from "@/components/header";
-import useUmiStore from "@/store/useUmiStore";
-import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
-import {
-  mplCandyMachine,
-  mintV1
-} from "@metaplex-foundation/mpl-core-candy-machine";
-import { 
-  publicKey as pubKey,
-  generateSigner, 
-  some 
-} from "@metaplex-foundation/umi";
-import { useWallet } from "@solana/wallet-adapter-react";
-import { useState,useEffect } from "react";
 import FetchCandyMachine  from "./fetchCandyMachine";
+import MintOneButton from "./mintOne";
 
 export default function Candy() {
-  const [mintLoading, setMintLoading] = useState(false)
   const candyMachineId = process.env.NEXT_PUBLIC_CANDYMACHINE_ID
-
-  const wallet = useWallet()
-  const umi = useUmiStore().umi
-  umi.use(walletAdapterIdentity(wallet))
-  umi.use(mplCandyMachine())
-  
   
   const candyMachine = FetchCandyMachine()
-  const mintOne = async () => {
-    try {
-      setMintLoading(true)
-      const nftMint = generateSigner(umi);
-      await mintV1(umi, {
-        candyMachine: pubKey(candyMachineId!),
-        collection: pubKey(candyMachine.collectionMint),
-        asset: nftMint,
-        candyGuard: pubKey(candyMachine.mintAuth),
-        mintArgs: {
-          solPayment: some({destination: pubKey(candyMachine.solPayDestination)})
-        }
-      }).sendAndConfirm(umi).then(() => {
-        setMintLoading(false)
-        console.log(`NFT ${nftMint.publicKey} minted!`)
 
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <Header />
@@ -54,15 +15,7 @@ export default function Candy() {
         <h2 className="text-center">{`You are about to mint from the candy machine ${candyMachineId!.slice(0,15)}...`}</h2>
         <p>{`${candyMachine.itemsRedeemed}/${candyMachine.itemsLoaded} items are already minted!`}</p>
         <div className="flex items-center justify-center">
-          <button onClick={()=> mintOne()} 
-            disabled={!candyMachine.allowed || mintLoading || candyMachineId===''}
-            className=" bg-[#512ba8] h-12 px-6 py-0 rounded font-bold text-white disabled:bg-slate-400">
-            {
-              !candyMachine.allowed? 'Mint not allowed!' :
-              mintLoading? 'Wait a sec...' :
-              'Mint'
-            }
-          </button>
+          <MintOneButton candyMachineId={candyMachineId!} collectionMint={candyMachine.collectionMint} mintAuth={candyMachine.mintAuth} solPayDestination={candyMachine.solPayDestination} allowed={candyMachine.allowed} />
         </div>
       </div>
       <p>{`An on-chain quest to find another earth, before it's too late! 🧑‍🚀🚀`}</p>
