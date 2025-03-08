ae0f0dc6499b25b79f826b715191efbe1948c241
keyhansa <keyhan.ahmad@gmail.com>
2025-03-08 13:15:39 +0330

    made fetchAssets a component

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,46 +1,36 @@
 "use client"
 import Header from "@/components/ui/header";
-import useUmiStore from "@/store/useUmiStore";
-import { dasApi } from '@metaplex-foundation/digital-asset-standard-api';
-import { das }  from '@metaplex-foundation/mpl-core-das';
-import { publicKey } from '@metaplex-foundation/umi';
-import { useState,useEffect } from "react";
-import { AssetResult } from "@metaplex-foundation/mpl-core-das/dist/src/types";
+import FetchAssets from "@/components/mpl/fetchAssets";
+import Link from "next/link";
+import {MdLink} from 'react-icons/md'
 
 export default function Collection() {
-  const [collection, setCollection] = useState({})
-  const [assets,setAssets] = useState<AssetResult[]>([])
-  const collectionId = publicKey(process.env.NEXT_PUBLIC_COLLECTION_ID!)
-  const umi = useUmiStore().umi
-  umi.use(dasApi())
-  
-  // const fetchCollection = async () => {
-  //   await das.getCollection(umi,collectionId).then((response) => {
-  //     setCollection(response)
-  //     console.log(response)
-  //   })
-  // }
-
-  const fetchAssets = async () => {
-    await das.getAssetsByCollection(umi, {collection: collectionId}).then((response) => {
-      setAssets(response)
-    })
-  }
-  useEffect(() => {
-    fetchAssets()
-  },[])
-
-  useEffect(() => {
-    console.log(assets)
-  }, [assets])
-
-
+  const collection = FetchAssets()
+  // console.log(collection)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8">
       <Header />
       <div className="flex flex-col gap-2 items-center text-sm">
-        <h2 className="text-center">{`You are reading data for ${collectionId!.slice(0,15)}...`}</h2>
-
+        <h2 className="text-center">{`You are reading data for ${collection.collectionId.slice(0,15)}...`}</h2>
+        <ul className="w-full text-xs mt-2">
+          {collection.assets.map((asset,index) => {
+            return (
+              <li key={index} className="mb-2 border-b-2">
+                <div className="flex flex-row justify-between w-full">
+                  <p>{asset.name}</p>
+                  <Link 
+                    href={`https://solana.fm/address/${asset.owner}/transactions?cluster=devnet-alpha`} 
+                    target="_blank">
+                    <div className="flex flex-row gap-1 items-center">
+                      <p>{asset.owner.slice(0,8)}</p>
+                      <MdLink />
+                    </div>
+                  </Link>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
       </div>
       <p>{`An on-chain quest to find another earth, before it's too late! 🧑‍🚀🚀`}</p>
     </main>
---
+++ src/components/mpl/fetchAssets.ts
@@ -0,0 +1,27 @@
+'use client'
+
+import useUmiStore from "@/store/useUmiStore";
+import { dasApi } from '@metaplex-foundation/digital-asset-standard-api';
+import { das }  from '@metaplex-foundation/mpl-core-das';
+import { publicKey } from '@metaplex-foundation/umi';
+import { useState,useEffect } from "react";
+import { AssetResult } from "@metaplex-foundation/mpl-core-das/dist/src/types";
+
+export default function FetchAssets() {
+    const [assets,setAssets] = useState<AssetResult[]>([])
+    const collectionId = publicKey(process.env.NEXT_PUBLIC_COLLECTION_ID!)
+    const umi = useUmiStore().umi
+    umi.use(dasApi())
+    
+    const fetchAssets = async () => {
+        await das.getAssetsByCollection(umi, {collection: collectionId}).then((response) => {
+          setAssets(response)
+        })
+    }
+
+    useEffect(() => {
+        fetchAssets()
+    })
+
+    return {assets,collectionId}
+}
