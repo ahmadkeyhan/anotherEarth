812a6bd0c581716da1b5cdc2b4d94afb4b8abbbe
keyhansa <keyhan.ahmad@gmail.com>
2025-03-13 12:01:58 +0330

    tidy up hero section

---
+++ src/app/showcaseItems.ts
@@ -0,0 +1,17 @@
+export const showcaseItems = [
+    {
+      name: "Astronaut #1",
+      price: "1.2 SOL",
+      image: "/astro1.jpg",
+    },
+    {
+      name: "Astronaut #2",
+      price: "0.8 SOL",
+      image: "/astro2.jpg",
+    },
+    {
+      name: "Astronaut #3",
+      price: "2.5 SOL",
+      image: "/astro3.jpg",
+    },
+]
